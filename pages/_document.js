7621aa8cb55242a698f0ca51948b13039a9bdca2
Masonball20 <MAS2162231@maricopa.edu>
2025-12-06 19:53:04 -0800

    Prioritize favicon.ico as primary icon
    - Remove PNG favicon links, use favicon.ico as primary
    - Keep apple-touch-icon and manifest for PWA support
    - favicon.ico contains multiple sizes (16x16, 32x32, 48x48, 256x256)

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,6 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#1c2d3f" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
 
